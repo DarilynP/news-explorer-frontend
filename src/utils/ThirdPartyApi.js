4f64f907c0e6fb1d324cef71d9f315eea76baad9
Darilyn <darilyn.peguero@gmail.com>
2025-09-20 00:22:32 -0400

    api upgrade

--- src/utils/ThirdPartyApi.js
+++ src/utils/ThirdPartyApi.js
@@ -1,7 +1,7 @@
 const API_KEY = "c608435d13a646328c916afd1a913da9";
 
 export function getNews(search) {
-  const url = `https://newsapi.org/v2/everything?pageSize=10&apiKey=${API_KEY}&q=${search}`;
+  const url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=10&apiKey=${API_KEY}&q=${search}`;
   return fetch(url)
     .then((res) => {
       if (!res.ok) {
